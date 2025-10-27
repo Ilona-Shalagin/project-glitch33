823880f280f0ca5d1939c35fd853843265611642
Ilona  Shalagin <ilona.shalagin25@gmail.com>
2025-10-27 16:20:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,136 @@
+const button = document.querySelector("#btn");
+const par = document.querySelector("#par");
+
+const prediction = [
+    "You'll communicate with dead relatives via virtual reality.",
+    "Your kitchen will restock itself.",
+    "You'll check email with your contact lenses.",
+    "We'll be communicating with thoughts.",
+    "The days will get a lot longer.",
+    "Pills will be able to detect cancer.",
+    "Bathroom mirrors will inspect your moles.",
+    "The robots are indeed coming.",
+    "Nanobots in your bloodstream will protect you from getting sick.",
+    "You'll be able to smell your favorite TV shows.",
+]
+
+button.addEventListener("click", function(){
+    let randomPrediction = prediction[Math.floor(Math.random()*prediction.length)];
+    console.log(randomPrediction);
+
+    par.textContent = randomPrediction;
+    par.style.display = "block";
+})
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 200,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+
+gsap.from("#btn",{rotation:360,scale:-2,opacity:0,duration:2,delay:.3})
+
